fb24b9d81582448ca0e9407599a128c76198b4dc
Jonathan Berrisch <Jonathan@Berrisch.biz>
2026-01-08 16:38:08 +0100

    Make get_postman script more verbose

--- misc/get_postman.sh
+++ misc/get_postman.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-curl "https://documenter.getpostman.com/api/collections/7009892/2s93JtP3F6" | jq > ./misc/TransparencyPlatformRestfulAPI.postman_collection.json
+echo "Fetching Postman collection..."
+curl -v "https://documenter.getpostman.com/api/collections/7009892/2s93JtP3F6" | jq > ./misc/TransparencyPlatformRestfulAPI.postman_collection.json
+echo "Response received. File size:"
+ls -lh ./misc/TransparencyPlatformRestfulAPI.postman_collection.json
+echo "First 500 characters of response:"
+head --lines=50 ./misc/TransparencyPlatformRestfulAPI.postman_collection.json
